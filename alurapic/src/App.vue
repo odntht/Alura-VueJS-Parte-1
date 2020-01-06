0b6c6afc00ad64cc13c9a0b9b11435ba37bd7bbc
Fernando <odntht@gmail.com>
2020-01-06 18:45:47 -0300

    UPDATE - Finalizando Aula 4 (Componentes e Encapsulamento de View)

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,33 +1,69 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
+  <div class="corpo">
 
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo" />
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+
+      <li class="lista-fotos-item" v-for="foto in fotos">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
       </li>
     </ul>
+
   </div>
 </template>
 
 <script>
+import Painel from "./components/shared/painel/Painel.vue";
+
 export default {
+  components: {
+    "meu-painel": Painel
+  },
+
   data() {
     return {
       titulo: "Alurapic",
+
       fotos: []
     };
   },
-
   created() {
-    this.$http.get("http://localhost:3000/v1/fotos") //Faz a requisição GET
-    .then(res => res.json()) //Quando a requisição retorna, salva esse resultado em 'res' e converte para json através do 'res.json()'
-    .then(fotos => this.fotos = fotos, err => console.log(err)); 
+    this.$http
+      .get("http://localhost:3000/v1/fotos") //Faz a requisição GET
+      .then(res => res.json()) //Quando a requisição retorna, salva esse resultado em 'res' e converte para json através do 'res.json()'
+      .then(fotos => (this.fotos = fotos), err => console.log(err));
     //Quando a res.json() retorna, a resposta dela é definida como a 'fotos' e salva no objeto através de 'this.fotos'
     // E caso não receba retorno, é apresentado um erro no console através do 'console.log(err)'
+    
   }
 };
 </script>
 
 <style>
+.titulo {
+  text-align: center;
+}
+
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
 </style>
