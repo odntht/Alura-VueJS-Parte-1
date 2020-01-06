7689370d8348085008df012e423f79b94d262515
Fernando <odntht@gmail.com>
2020-01-06 15:52:45 -0300

    UPDATE - Finalizando Aula 3 (Integracao com APIs)

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -4,35 +4,29 @@
 
     <ul>
       <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+        <img :src="foto.url" :alt="foto.titulo" />
       </li>
     </ul>
-
   </div>
 </template>
 
 <script>
 export default {
-
   data() {
-
     return {
+      titulo: "Alurapic",
+      fotos: []
+    };
+  },
 
-      titulo: 'Alurapic', 
-      fotos: [
-        {
-          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
-          titulo: 'cachorro'
-        },
-        {
-          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
-          titulo: 'Cachorrão'
-        }
-      ]
-    }
+  created() {
+    this.$http.get("http://localhost:3000/v1/fotos") //Faz a requisição GET
+    .then(res => res.json()) //Quando a requisição retorna, salva esse resultado em 'res' e converte para json através do 'res.json()'
+    .then(fotos => this.fotos = fotos, err => console.log(err)); 
+    //Quando a res.json() retorna, a resposta dela é definida como a 'fotos' e salva no objeto através de 'this.fotos'
+    // E caso não receba retorno, é apresentado um erro no console através do 'console.log(err)'
   }
-}
-
+};
 </script>
 
 <style>
