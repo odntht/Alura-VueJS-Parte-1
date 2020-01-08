fb0db4a673ad4488e1653b9da168e3d4d00480ac
Fernando <odntht@gmail.com>
2020-01-08 16:29:48 -0300

    UPDATE - FInalizando Aula 7 (Mais sobre Componentes) e Finalizando Parte 1 do Curso

--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -15,6 +15,13 @@
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+          <meu-botao
+            tipo="button"
+            rotulo="REMOVER"
+            @botaoAtivado="remove(foto)"
+            :confirmacao="true"
+            estilo="perigo"
+          />
         </meu-painel>
       </li>
     </ul>
@@ -24,11 +31,13 @@
 <script>
 import Painel from "../shared/painel/Painel.vue";
 import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
+import Botao from "../shared/botao/Botao.vue";
 
 export default {
   components: {
     "meu-painel": Painel,
-    "imagem-responsiva": ImagemResponsiva
+    "imagem-responsiva": ImagemResponsiva,
+    "meu-botao": Botao
   },
 
   data() {
@@ -51,6 +60,14 @@ export default {
       } else {
         return this.fotos;
       }
+    }
+  },
+
+  methods: {
+    remove(foto) {
+      // remove($event, foto) {
+      // alert($event);
+      alert("Remover a foto " + foto.titulo);
     }
   },
 
---
+++ alurapic/src/components/shared/botao/Botao.vue
@@ -0,0 +1,69 @@
+<template>
+  <button @click="disparaAcao()" class="botao" :class="estiloDoBotao" :type="tipo">{{ rotulo }}</button>
+</template>
+
+<script>
+
+export default {
+  // Validando as propriedades (props) do elemento button
+  props: {
+
+    tipo: {
+      required: true,
+      type: String
+    },
+
+    rotulo: {
+      required: true,
+      type: String
+    },
+
+    confirmacao: Boolean,
+    estilo: String
+    
+  },
+
+  methods: {
+    disparaAcao() {
+      if (this.confirmacao) {
+        if (confirm("Confirma?")) {
+          // this.$emit("botaoAtivado", new Date());
+          this.$emit("botaoAtivado");
+        }
+        // else {
+        //   alert("Foto NÃO removida!");
+        // }
+        return;
+      }
+      this.$emit("botaoAtivado");
+    }
+  },
+
+  computed: {
+    estiloDoBotao() {
+      if (this.estilo == "padrao" || !this.estilo) return "botao-padrao";
+      if (this.estilo == "perigo") return "botao-perigo";
+    }
+  }
+};
+</script>
+
+<style scoped>
+.botao {
+  display: inline-block;
+  padding: 10px;
+  border-radius: 3px;
+  margin: 10px;
+  font-size: 1.2em;
+}
+
+.botao-perigo {
+  background: firebrick;
+  color: white;
+}
+
+.botao-padrao {
+  background: darkcyan;
+  color: white;
+}
+</style>
